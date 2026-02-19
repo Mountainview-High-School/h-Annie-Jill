e0fcb1c93ddffa75f0476ba1be57968451af7619
Annie-Jill <24evansa@mountainview.school.nz>
2026-02-20 10:40:16 +1300

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,16 @@
 import tkinter as tk
+
+def print_text():
+    print("helo welcom")
+    label.config(text=click_button)
+
 root = tk.Tk()
-label = tk.Label(root, text="helo welcome")
-label.pack()
-button = tk.Button(root, text="click", width=25,)
+
+button = tk.Button(root, text="click", width=25, command=click_button)
 button.pack()
+
+label= tk.Label(root, text="")
+label.pack(pady=10)
+
+
+root.mainloop()

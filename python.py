75f82de5a82fee4b6a74cc29412ba0636db57702
Annie-Jill <24evansa@mountainview.school.nz>
2026-02-20 10:19:44 +1300

    ok

--- python.py
+++ python.py
@@ -1,9 +1,6 @@
 import tkinter as tk
-
 root = tk.Tk()
-
 label = tk.Label(root, text="helo welcome")
 label.pack()
 button = tk.Button(root, text="click", width=25,)
 button.pack()
-root.mainloop()

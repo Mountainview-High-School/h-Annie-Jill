3f5e416fc60a9a1c9e6841b17509dc28dd72a814
Annie-Jill <24evansa@mountainview.school.nz>
2026-03-06 10:35:18 +1300

    things

---
+++ sql.py
@@ -0,0 +1,12 @@
+import os
+os.system('cls')
+import sqlite3
+connection = sqlite3.connect("newdb.db")
+cursor = connection.cursor()
+cursor.execute("SELECT first_name, last_name FROM student")
+
+students = cursor.fetchall()
+print("All students")
+for student in students:
+    print(student)
+connection.close()
